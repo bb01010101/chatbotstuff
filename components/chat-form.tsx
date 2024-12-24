fc7f4f9af7b98dc9af4d5318214603681a3f2ba8
bb01010101 <bb7906@princeton.edu>
2024-12-24 13:57:26 -0500

    test

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -33,7 +33,7 @@ export function ChatForm({
   const header = (
     <header className="m-auto flex max-w-96 flex-col gap-5 text-center">
       <h1 className="text-2xl font-semibold leading-none tracking-tight">
-        Basic AI Chatbot Template
+        Brian Boler's Chatbot
       </h1>
       <p className="text-muted-foreground text-sm">
         This is an AI chatbot app template built with{' '}
